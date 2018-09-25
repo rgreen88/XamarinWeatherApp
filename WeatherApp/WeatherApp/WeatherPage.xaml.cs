12f485e4d4a9c7ea2a38ec11c342f12dcc5e6746
rgreen88 <rynelgreen@gmail.com>
2018-09-25 15:20:57 -0400

    completed WeatherPage.xaml and cs. Adding app launch to designate starting page.

--- WeatherApp/WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -1,20 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace WeatherApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class WeatherPage : ContentPage
-	{
-		public WeatherPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class WeatherPage : ContentPage
+    {
+        //TODO: Add app context on app start to display weather page
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+           
+            BindingContext = new Weather();
+        }
+
+        //Added on click event for button to fetch zipcode weather
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            Weather weather = await Core.GetWeather("30519");//Buford, Ga
+            getWeatherBtn.Text = weather.Title;
+        } //Set the default page on app load 
+    }
 }
