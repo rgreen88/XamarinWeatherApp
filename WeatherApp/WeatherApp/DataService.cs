96cf8900c199efd7b211502ed2405d42a1e32e88
rgreen88 <rynelgreen@gmail.com>
2018-09-25 14:28:12 -0400

    Created DataService to take in a string param for JSON deserialization

---
+++ WeatherApp/WeatherApp/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+//processing JSON here using Http, Tasks, and Json
+namespace WeatherApp
+{
+    class DataService
+    {
+        //string param for query using Http client object
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            //client object holding queryString to deserialize JSON object
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            //if data exists, asynchronously deserialize each object from JSON
+            //until fulfilled
+            dynamic data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            //data stored deserialized JSON data for display use
+            return data;
+        }
+    }
+}
